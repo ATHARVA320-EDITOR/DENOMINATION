8e107f21225ad02c49a7dbb2238fda2a02aa8891
ATHARVA320-EDITOR <ddnatharva@gmail.com>
2025-02-13 16:56:27 +0530

    intial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+root = Tk()
+root.title("Denomination calculator")
+root.configure(bg = 'light blue')
+root.geometry = ('650x400')
+upload = Image.open("img.jpeg")
