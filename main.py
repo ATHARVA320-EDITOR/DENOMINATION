5ce917c3443984c5bb50a7f71431b9f86a1a9f09
ATHARVA320-EDITOR <ddnatharva@gmail.com>
2025-02-20 16:58:19 +0530

    second commit

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ root.title("Denomination calculator")
 root.configure(bg = 'light blue')
 root.geometry = ('650x400')
 upload = Image.open("img.jpeg")
+upload = upload.resize((300, 300))
+image = ImageTk.PhotoImage(upload)
+label = Label(root, image=image, bg='light blue')
+label.place(x=100, y=20)
+label1 = Label(root,
+               text="Hey user!Welcome to denomination calculator app.",
+               bg='light blue')
+label1.place(relx= 0.5, y= 340, anchor=CENTER)
+def msg():
+    MsgBox = messagebox.showinfo(
+        "Alert", "Do you want to calculate the denomination?")
+    if MsgBox == 'ok':
+        topwin()
